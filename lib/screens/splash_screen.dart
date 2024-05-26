dff82bea7da16d9d3073fb83fff1107c53afad7e
@erick <erickescribaa@gmail.com>
2024-05-25 20:03:59 -0500

    chore: add splash screen (#1)

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:invest_app/screens/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (_) => const HomeScreen(),
+      ));
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          colors: [Colors.blue, Colors.white],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        )),
+        child: const Image(
+          image: AssetImage('assets/splash_logo.png'),
+        ),
+      ),
+    );
+  }
+}
