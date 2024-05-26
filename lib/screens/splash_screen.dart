4d80d2f86f81d8ca5389720a2f699b6e592fb4f5
@erick <erickescribaa@gmail.com>
2024-05-26 14:37:31 -0500

    chore: add welcome screen (#2)

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:invest_app/screens/home_screen.dart';
+import 'package:invest_app/screens/auth/welcome_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -16,7 +16,7 @@ class _SplashScreenState extends State<SplashScreen> {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
     Future.delayed(const Duration(seconds: 2), () {
       Navigator.of(context).pushReplacement(MaterialPageRoute(
-        builder: (_) => const HomeScreen(),
+        builder: (_) => const WelcomeScreen(),
       ));
     });
   }
