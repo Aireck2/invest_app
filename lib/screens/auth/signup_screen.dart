1cd41790bb84a2c55c2c77b6333a5ba053493cdd
@erick <erickescribaa@gmail.com>
2024-05-26 19:00:59 -0500

    chore: add signup screen (#3)

--- lib/screens/auth/signup_screen.dart
+++ lib/screens/auth/signup_screen.dart
@@ -1,12 +1,127 @@
 import 'package:flutter/material.dart';
+import 'package:invest_app/screens/auth/welcome_screen.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Sign up '),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.symmetric(vertical: 75, horizontal: 25),
+          child: Column(
+            children: [
+              const Text(
+                'Create an account',
+                style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold),
+              ),
+              const Text(
+                'Invest and double your income now',
+              ),
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      expands: false,
+                      decoration: const InputDecoration(
+                        labelText: 'First name',
+                        prefixIcon: Icon(Icons.person_outline),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: TextFormField(
+                      expands: false,
+                      decoration: const InputDecoration(
+                        labelText: 'Last name',
+                        prefixIcon: Icon(Icons.person_outline),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  prefixIcon: Icon(Icons.email_outlined),
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Phone',
+                  prefixIcon: Icon(Icons.phone_outlined),
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextFormField(
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  suffixIcon: Icon(Icons.visibility_off_outlined),
+                  prefixIcon: Icon(Icons.lock_outline),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  Checkbox(value: true, onChanged: (value) {}),
+                  const Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(text: 'I agree to '),
+                        TextSpan(
+                          text: 'Privacy policy ',
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                        TextSpan(text: 'and '),
+                        TextSpan(
+                          text: 'Terms of use',
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              const SizedBox(height: 20),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                    backgroundColor: Colors.green,
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 80)),
+                child: const Text(
+                  'Create account',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(builder: (_) => const WelcomeScreen()));
+                },
+                style: TextButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 80)),
+                child: const Text(
+                  'Already has an account?',
+                  style: TextStyle(color: Colors.green),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
